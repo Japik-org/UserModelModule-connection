8c8ee27cbe252746dbbf60e391deedc850b1a386
pro100kryto <pro100kryto.original@gmail.com>
2022-01-13 15:45:10 -0300

    Create project

---
+++ src/main/java/com/pro100kryto/server/modules/usermodel/connection/IUserModelModuleConnection.java
@@ -0,0 +1,7 @@
+package com.pro100kryto.server.modules.usermodel.connection;
+
+import com.pro100kryto.server.module.IModuleConnection;
+
+public interface IUserModelModuleConnection extends IModuleConnection {
+
+}
